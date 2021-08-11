f9441d0356be1b1fa2caf28dbfb2605a72d9a986
Alyssa Antonaccio <alyantonaccio@gmail.com>
2021-08-11 10:31:14 -0400

    added first sample test

---
+++ frontend/cypress/integration/first_test.js
@@ -0,0 +1,12 @@
+// first_test.js created with Cypress
+//
+// Start writing your Cypress tests below!
+// If you're unfamiliar with how Cypress works,
+// check out the link below and learn how to write your first test:
+// https://on.cypress.io/writing-first-test
+
+describe('My first test', () => {
+  it('does not do much', () => {
+    expect(true).to.equal(true)
+  })
+})
